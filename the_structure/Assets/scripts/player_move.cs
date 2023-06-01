81c7e72eeaadb73af3672d01adedfe7bf90893cb
CKYKA513 <d.robberts911@gmail.com>
2023-06-01 10:43:02 -0400

    fixed player move
    player_move script working now

--- the_structure/Assets/scripts/player_move.cs
+++ the_structure/Assets/scripts/player_move.cs
@@ -4,57 +4,19 @@ using UnityEngine;
 
 public class player_move : MonoBehaviour
 {
-    //public Vector2 speed;
-    Rigidbody2D rigidBody;
-    public float speed = 10f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidBody = gameObject.GetComponent<Rigidbody2D>();
-    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 pos = transform.position;
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
 
-        // move left
-        if (Input.GetKeyDown("d"))
-        {
-            Debug.Log("left");
-            pos.x += speed;
-            rigidBody.AddForce(pos, ForceMode2D.Force);
-        }
-
-        // move right
-        if (Input.GetKeyDown("a"))
-        {
-            Debug.Log("right");
-            pos.x -= speed;
-            rigidBody.AddForce(pos, ForceMode2D.Force);
-        }
-
-        // move down
-        if (Input.GetKeyDown("w"))
-        {
-            Debug.Log("up");
-            pos.y += speed;
-            rigidBody.AddForce(pos, ForceMode2D.Force);
-        }
-
-        // move down
-        if (Input.GetKeyDown("s"))
-        {
-            Debug.Log("down");
-            pos.y -= speed;
-            rigidBody.AddForce(pos, ForceMode2D.Force);
-        }
-
-        // stop
-        if (Input.GetKeyUp("w") || Input.GetKeyUp("a") || Input.GetKeyUp("s") || Input.GetKeyUp("d"))
-        {
-            {
-            }
-        }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
